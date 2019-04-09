6f1e5d8e289f13484418f5e066b172741edfe73b
Raj Kathal <rajkathal.skd@gmail.com>
2019-04-09 22:25:48 +0530

    Till part 3 from Acadminds

---
+++ api/routes/products.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+
+//GET
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET Request to /products'
+    });
+});
+
+//POST
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'Handling POST Request to /products'
+    });
+});
+
+// Get with ID
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if (id === 'special') {
+        res.status(200).json({
+            message: 'You discovered the special ID',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID'
+        });
+    }
+});
+
+// PATCH
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated product'
+    });
+});
+// DELETE
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Deleted product'
+    });
+});
+
+module.exports = router;
